de67964dcb3e36a5da902da4e22e8a8171defce1
yanqirenshi <yanqirenshi@gmail.com>
2022-08-06 11:55:12 +0900

    add material ui

--- dev/src/PageRepositories.js
+++ dev/src/PageRepositories.js
@@ -1,29 +1,17 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from './slices/counterSlice.js';
+
+import GlobalAppBar from './components/GlobalAppBar.js';
+import Repositories from './components/Repositories.js';
 
 export default function PageRepositories () {
-    const count = useSelector(state => state.counter.value);
-    const dispatch = useDispatch();
-
     return (
         <div>
+          <GlobalAppBar title="Repositories"/>
 
-          <div>
-            <button aria-label="Increment value"
-                    onClick={() => dispatch(increment())}>
-              +
-            </button>
-
-            <span>{count}</span>
-
-            <button aria-label="Decrement value"
-                    onClick={() => dispatch(decrement())}>
-              -
-            </button>
+          <div style={{margin:22}}>
+            <Repositories />
           </div>
-
         </div>
     );
 }
