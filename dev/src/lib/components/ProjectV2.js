99410395bda5dc4d2602049b7b3035da40f0eca3
岩崎仁是 <yanqirenshi@gmail.com>
2023-10-12 10:51:24 +0900

    ちょっと枠を書いた。 #4

---
+++ dev/src/lib/components/ProjectV2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+
+export default function ProjectV2 (props) {
+    const sogh = props.sogh;
+
+    return (
+        <Box sx={{pt:2, pb: 22}}>
+          <Box>Title</Box>
+
+          <Box>Attributes</Box>
+
+          <Box>Details</Box>
+        </Box>
+    );
+}
--- dev/src/pages/SoghProject.js
+++ dev/src/pages/SoghProject.js
@@ -3,6 +3,8 @@ import React from 'react';
 // import { useRecoilState } from "recoil";
 // import * as atoms from '../recoil/ATOMS.js';
 
+import Container from '@mui/material/Container';
+
 import Frame from '../assemblies/frames/Frame.js';
 
 // import Wbs from '../panels/Wbs.js';
@@ -13,12 +15,21 @@ import Frame from '../assemblies/frames/Frame.js';
 // import Parts from '../panels/design/Parts.js';
 // import Events from '../panels/design/Events.js';
 
+import {ProjectV2} from '../lib/index.js';
+
 export default function SoghProject () {
     // const [page_data, setPageData] = useRecoilState(atoms.PAGE_DESIGN);
 
+    /*
+      1. Project のデータを取得する。
+      2. Project Items を取得する。
+     */
+
     return (
         <Frame>
-          <p>XXXXXXXXXXXXXXX</p>
+          <Container maxWidth="lg">
+            <ProjectV2/>
+          </Container>
         </Frame>
     );
 }
