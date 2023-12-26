b85f8fb017fe6d02d08dce0f6d52f41434d3162a
yanqirenshi <yanqirenshi@gmail.com>
2023-12-26 13:40:54 +0900

    var 0.1.23

--- src/js/models/PullRequest.js
+++ src/js/models/PullRequest.js
@@ -1,7 +1,26 @@
 import GraphQLNode from './GraphQLNode.js';
 
 export default class PullRequest extends GraphQLNode {
+    title () {
+        return this._core.number || null;
+    }
+    number () {
+        return this._core.number || null;
+    }
     url () {
         return this._core.url || null;
+    }
+    body () {
+        return this._core.body || '';
+    }
+    bodyHTML () {
+        return this._core.bodyHTML || '';
+    }
+    author () {
+        return this._core.number || null;
+    }
+    assignees () {
+        const edges = this._core.assignees.edges;
+        return edges.map(e=>e.node);
     }
 }
