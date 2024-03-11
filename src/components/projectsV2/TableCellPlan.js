c052c798f7974e7788483c3ad44b529ee887ca11
yanqirenshi <yanqirenshi@gmail.com>
2024-03-12 08:42:19 +0900

    var 0.2.3

--- src/components/projectsV2/TableCellPlan.js
+++ src/components/projectsV2/TableCellPlan.js
@@ -25,23 +25,23 @@ export default function TableCellPlan (props) {
             </>
         );
 
-    const is_in_term = start.isSameOrAfter(now) && end.isSameOrBefore(now);
-    const is_passed = end.isAfter(now);
+    const is_in_term = now.isSameOrAfter(start) && now.isSameOrBefore(end);
+    const is_passed = now.isAfter(end);
 
     const sx = {
         whiteSpace: 'nowrap',
-        fontWeight:is_in_term || is_passed ? 'bold' : null,
+        fontWeight: is_in_term || is_passed ? 'bold' : null,
         color: is_passed ? '#ec6d71' : null,
     };
 
     return (
         <>
           <Cell sx={sx} title={plan.start}>
-            {start.format('YYYY-MM-DD')}
+            {start.format('MM-DD')}
           </Cell>
 
           <Cell sx={sx} title={plan.end}>
-            {end.format('YYYY-MM-DD')}
+            {end.format('MM-DD')}
           </Cell>
         </>
     );
